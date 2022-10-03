baf89e7f86108997294a017bcc6fa4fa72a80724
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-03 19:14:34 +0300

    Не работает

--- src/main/java/thread/Buyer.java
+++ src/main/java/thread/Buyer.java
@@ -3,38 +3,49 @@ package thread;
 import avto.CarImp;
 
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Buyer extends Thread {
     private int time;
     private final List<CarImp> list;
     private Thread thread;
+    ReentrantLock locker;
+    Condition condition;
 
-    public Buyer(int time, List<CarImp> list, Thread thread) {
+    public Buyer(int time, List<CarImp> list, Thread thread, ReentrantLock lock, Condition condition) {
         this.time = time;
         this.list = list;
         this.thread = thread;
+        this.locker = lock;
+        this.condition = condition;
     }
 
     @Override
     public void run() {
-
-        do {
-            synchronized (list) {
+        locker.lock();
+        try {
+            while (thread.isAlive()) {
                 System.out.println(Thread.currentThread().getName() + " зашёл в магазин");
                 if (list.isEmpty()) {
                     System.out.println("Машин нет");
+                    condition.wait();
                 } else {
                     System.out.printf("%s купил %s.\n",
                             Thread.currentThread().getName(),
                             list.remove(0));
                 }
-                list.notify();
+                try {
+                    Thread.sleep(time);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            try {
-                Thread.sleep(time);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } while (thread.isAlive());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            locker.unlock();
+        }
     }
 }
+
