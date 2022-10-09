4affcab72dfbe34ab8f12693948e72335b984cf9
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-09 21:27:01 +0300

    Рефакторинг

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,21 @@
 import avto.CarImp;
 import avto.Toyota;
 import avto.Volvo;
-import thread.Buyer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-    public final static int ISSUE = 6;
-    public final static int TIME_ADMISSION = 3000;
-    public final static int TIME_PEOPLE_BUY = 2500;
+    public final static int ISSUE = 10;
+    public final static int TIME_ADMISSION = 1000;
+    public final static int TIME_PEOPLE_BUY = 2000;
 
     public static void main(String[] args) {
         List<CarImp> auto = new ArrayList<>();
         ReentrantLock locker = new ReentrantLock(true);
+        Condition condition = locker.newCondition();
         Random random = new Random();
 
         Thread carThread = new Thread(() -> {
@@ -25,28 +23,54 @@ public class Main {
             int value;
             for (int i = 0; i < ISSUE; i++) {
                 value = random.nextInt(3);
-//                locker.lock();
-//                try {
+                locker.lock();
+                try {
                     switch (value) {
                         case 0 -> auto.add(newCar = new Toyota("Camry", 2016));
                         case 1 -> auto.add(newCar = new Volvo("XC90", 2021));
                         case 2 -> auto.add(newCar = new Toyota("RAV4", 2019));
                     }
                     newCar.admission();
+                    condition.signal();
                     try {
                         Thread.sleep(TIME_ADMISSION);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-//                } finally {
-//                    locker.unlock();
-//                }
+                } finally {
+                    locker.unlock();
+                }
             }
+            Thread.currentThread().interrupt();
         });
         carThread.start();
 
-        for (int i = 1; i <= 4; i++) {
-            Thread buy = new Thread(new Buyer(TIME_PEOPLE_BUY, auto, carThread, locker));
+        Runnable buyer = () -> {
+            try {
+                do {
+                    locker.lock();
+                    System.out.println(Thread.currentThread().getName() + " зашёл в магазин");
+                    Thread.sleep(TIME_PEOPLE_BUY);
+                    if (auto.isEmpty()) {
+                        if (carThread.isInterrupted()) {
+                            System.out.printf("%s остановлен", Thread.currentThread().getName());
+                            Thread.currentThread().interrupt();
+                        }
+                        System.out.printf("%s встал в очередь на машину\n", Thread.currentThread().getName());
+                        condition.await();
+                    }
+                    System.out.printf("%s купил автомобиль - %s\n", Thread.currentThread().getName(), auto.remove(0));
+                    Thread.sleep(TIME_PEOPLE_BUY);
+//                    locker.unlock();
+                }
+                while (carThread.isAlive());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        };
+
+        for (int i = 1; i <= 5; i++) {
+            Thread buy = new Thread(buyer);
             buy.setName("Покупатель " + i);
             buy.start();
         }
